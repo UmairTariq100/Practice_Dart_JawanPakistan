0997e28ff3315990165f448fb93531bfe2cecfcd
UmairTariq100 <umairtariq573@gmail.com>
2024-08-25 16:39:53 +0500

    3rd class

---
+++ main.dart
@@ -0,0 +1,7 @@
+void main(){
+ String username = 'Umair';
+ String email = 'umair@gmail.com';
+ String password = '123456789';
+
+ print((username == 'Umair' && email == 'umair@gmail.com') && password == '123456789');
+}
